643cf17824f659bca6494653bb1897972ea9a28a
Aziz benhmida <benhmida.mohamedaziz@esprit.tn>
2023-09-20 20:17:40 +0100

    fff

--- builtins_list.c
+++ builtins_list.c
@@ -1,11 +1,11 @@
 #include "shell.h"
 
 /**
- * builtins_list - search for match and execute the associate builtin
- * @data: struct for the program's data
- * Return: Returns the return of the function executed is there is a match,
+ * builtins_list - searches for a match and executes the associated builtin function
+ * @data: struct containing the program's data
+ * Return: Returns the return value of the executed function if there is a match,
  * otherwise returns -1.
- **/
+ */
 int builtins_list(data_of_program *data)
 {
 	int iterator;
@@ -20,16 +20,12 @@ int builtins_list(data_of_program *data)
 		{NULL, NULL}
 	};
 
-/*walk through the structure*/
 	for (iterator = 0; options[iterator].builtin != NULL; iterator++)
 	{
-/*if there is a match between the given command and a builtin,*/
 		if (str_compare(options[iterator].builtin, data->command_name, 0))
 		{
-/*execute the function, and return the return value of the function*/
 			return (options[iterator].function(data));
 		}
-/*if there is no match return -1 */
 	}
 	return (-1);
 }
--- builtins_more.c
+++ builtins_more.c
@@ -1,20 +1,20 @@
 #include "shell.h"
 
 /**
- * builtin_exit - exit of the program with the status
- * @data: struct for the program's data
- * Return: zero if sucess, or other number if its declared in the arguments
+ * builtin_exit - exits the program with a specified status
+ * @data: struct containing the program's data
+ * Return: zero on success, or a different number if specified in the arguments
  */
 int builtin_exit(data_of_program *data)
 {
 	int i;
 
 	if (data->tokens[1] != NULL)
-	{/*if exists arg for exit, check if is a number*/
+	{
 		for (i = 0; data->tokens[1][i]; i++)
 			if ((data->tokens[1][i] < '0' || data->tokens[1][i] > '9')
 				&& data->tokens[1][i] != '+')
-			{/*if is not a number*/
+			{
 				errno = 2;
 				return (2);
 			}
@@ -25,9 +25,9 @@ int builtin_exit(data_of_program *data)
 }
 
 /**
- * builtin_cd - change the current directory
- * @data: struct for the program's data
- * Return: zero if sucess, or other number if its declared in the arguments
+ * builtin_cd - changes the current directory
+ * @data: struct containing the program's data
+ * Return: zero on success, or a different number if specified in the arguments
  */
 int builtin_cd(data_of_program *data)
 {
@@ -63,11 +63,12 @@ int builtin_cd(data_of_program *data)
 }
 
 /**
- * set_work_directory - set the work directory
- * @data: struct for the program's data
- * @new_dir: path to be set as work directory
- * Return: zero if sucess, or other number if its declared in the arguments
+ * set_work_directory - sets the working directory
+ * @data: struct containing the program's data
+ * @new_dir: path to be set as the working directory
+ * Return: zero on success, or a different number if specified in the arguments
  */
+
 int set_work_directory(data_of_program *data, char *new_dir)
 {
 	char old_dir[128] = {0};
@@ -90,9 +91,9 @@ int set_work_directory(data_of_program *data, char *new_dir)
 }
 
 /**
- * builtin_help - shows the environment where the shell runs
- * @data: struct for the program's data
- * Return: zero if sucess, or other number if its declared in the arguments
+ * builtin_help - displays information about the shell environment
+ * @data: struct containing the program's data
+ * Return: zero on success, or a different number if specified in the arguments
  */
 int builtin_help(data_of_program *data)
 {
@@ -101,7 +102,6 @@ int builtin_help(data_of_program *data)
 
 	mensajes[0] = HELP_MSG;
 
-	/* validate args */
 	if (data->tokens[1] == NULL)
 	{
 		_print(mensajes[0] + 6);
@@ -128,27 +128,26 @@ int builtin_help(data_of_program *data)
 			return (1);
 		}
 	}
-	/*if there is no match, print error and return -1 */
 	errno = EINVAL;
 	perror(data->command_name);
 	return (0);
 }
 
 /**
- * builtin_alias - add, remove or show aliases
- * @data: struct for the program's data
- * Return: zero if sucess, or other number if its declared in the arguments
+ * builtin_alias - adds, removes, or displays aliases
+ * @data: struct containing the program's data
+ * Return: zero on success, or a different number if specified in the arguments
  */
+
 int builtin_alias(data_of_program *data)
 {
 	int i = 0;
 
-	/* if there are no arguments, print all environment */
 	if (data->tokens[1] == NULL)
 		return (print_alias(data, NULL));
 
 	while (data->tokens[++i])
-	{/* if there are arguments, set or print each env variable*/
+	{
 		if (count_characters(data->tokens[i], "="))
 			set_alias(data->tokens[i], data);
 		else
